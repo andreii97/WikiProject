0ebcd8a4fdd0ad76ac605f3b613d8802dc9d8571
Andrei Ionescu <andreionescu@Andreis-MacBook-Pro.local>
2019-07-05 21:27:48 +0200

    update: already started on the decision tree

--- WikiStat/js/script.js
+++ WikiStat/js/script.js
@@ -6,12 +6,15 @@ $(document).ready(function () {
         doc = json;
         console.log(doc); // this will show the info it in firebug console
     });
+    var firstChoice;  //variables
+    var secondChoice;  // outcome type
+    var thirdChoice;  // predictor variables
+    var fourthChoice;  // predictor type
+    var fifthChoice;   // meet assumptions
+    var categorical1;   //
+    var categorical2;   //
 
 
-
-    var firstChoice;
-    var secondChoice;
-    var thirdChoice;
     $("#one").change(function () {
         firstChoice = $(this).children("option:selected").text();
         //alert("Great! Two more to go!");
@@ -22,25 +25,34 @@ $(document).ready(function () {
     });
     $("#three").change(function () {
         thirdChoice = $(this).children("option:selected").text();
-        alert("Awesome!");
+        //alert("Awesome!");
+    });
+    $("#four").change(function () {
+        fourthChoice = $(this).children("option:selected").text();
+        //alert("Awesome!");
+    });
+    $("#five").change(function () {
+        fifthChoice = $(this).children("option:selected").text();
+        //alert("Awesome!");
+    });
+    $("#catg").on('click', function () {
+        alert("Get ready for more questions!");
+        $(".fourthQuestion").append("<h3>How many categories</h3>" + "<select id='hidden'><option>Two</option><option>More Than Two</option></select>");
+        categorical1 = $(this).children("option:selected").text();
+        $(".fourthQuestion").append("<h3>Same Participants or Different Used in Each Category?</h3>" + "<select id='hidden1'><option>Different</option><option>Same</option></select>");
+        categorical2 = $(this).children("option:selected").text();
     });
 
     $('.btn').on('click', function () {
-        alert("So you chose " + firstChoice + " as type of data, " + secondChoice + " as number of variables, and " + thirdChoice + " as blablabla. ");
-        if (firstChoice === "Second Data" && secondChoice === "Second Number") {
-            alert("We have got just the method for you! It's " + doc.content[0]);
-            var answer = confirm('Do you want to read more about it ?');
-            if (answer) {
-                var info = doc.content[0];
-                alert(info);
-                if (window.confirm('Click ok for more infos on this method')) {
-                    window.location.href = 'https://en.wikipedia.org/wiki/Linear_regression';
-                }
-                ;
-            } else {
-                alert("As you wish.");
+        alert("So you chose " + firstChoice + " as number of variables, " + secondChoice + " as outcome type, and " + thirdChoice + " as number of predictor variables. The predictor is " + fourthChoice);
+        if(categorical1 != null && categorical2 != null) {
+            alert("Since you have chosen a categorical predictor, there will be " + categorical1 + "categories, and the participants will be " + categorical2 + " in each category." );
+        }
+        else {
+            alert("That's it! We are now analyzing your inputs and trying to find the best methods for you!");
+            if(firstChoice === "One" && secondChoice === "Continuous" && thirdChoice === "One" && fourthChoice === "Continuous" && fifthChoice === "Yes") {
+                alert("Great! The method you should use is " + doc.content[1].Method + ". Do you want to read more about it? ");
             }
-
         }
     });
     $("#bla").on("click", function () {
