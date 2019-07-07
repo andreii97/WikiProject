aec0ac42eb8b077ab99d9dcaeccafa3997cbabfb
Andrei Ionescu <andreionescu@Andreis-MacBook-Pro.local>
2019-07-08 00:53:57 +0200

    update: decision tree almost done

--- WikiProject-master3/WikiStat/js/script2.js
+++ WikiProject-master3/WikiStat/js/script2.js
@@ -6,16 +6,4 @@ $(document).ready(function () {
             $('body').append('<p><span><h4>Method:</h4></span> ' + f.Method + '<br><span><h4>Description:</h4></span> ' + f.Description + '</p>');
         });*/
     });
-    $("#linearReg").one('click', function () {
-        console.log(doc.content[0]);
-        $('body').append('<p><span><h4>Method:</h4></span> ' + doc.content[0].Method + '<br><span><h4>Description:</h4></span> ' + doc.content[0].Description + '</p>');
-    });
-    $("#pearson").one('click', function () {
-        console.log(doc.content[1]);
-        $('body').append('<p><span><h4>Method:</h4></span> ' + doc.content[1].Method + '<br><span><h4>Description:</h4></span> ' + doc.content[1].Description + '</p>');
-    });
-    $("#chi").one('click', function () {
-        console.log(doc.content[2]);
-        $('body').append('<p><span><h4>Method:</h4></span> ' + doc.content[2].Method + '<br><span><h4>Description:</h4></span> ' + doc.content[2].Description + '</p>');
-    });
 });
